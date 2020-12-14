b1f5cee655c32f47fa84ed8d686ccb7d3b32219a
fjoseph1 <75974481+fjoseph1@users.noreply.github.com>
2020-12-14 09:42:23 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,274 @@
+<!DOCTYPE html> 
+
+<html> 
+
+    <head> 
+        
+
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1"> 
+        <link rel="stylesheet" href="style.css" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+        
+        
+
+
+
+        <title>Ma page Web</title> 
+
+    </head> 
+
+  
+    <body>
+        <header> 
+        <nav class="navbar navbar-expand-lg navbar fixed-top navbar navbar-transparent navbar-dark bg-dark " color-on-scroll="400">
+              <a class="navbar-brand" style="margin-left:10%" href="#">Franck JOSEPH</a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse justify-content-end"style="margin-right:10%" id="navigation">
+
+                <div class="navbar-nav"> 
+                  <a class="nav-item nav-link active" href="#">A propos <span class="sr-only">(current)</span></a>
+                  <a class="nav-item nav-link" href="#">Projets</a>
+                  <a class="nav-item nav-link" href="#">Contact</a>
+                  <a class="nav-item nav-link disabled" href="#">Disabled</a>
+                    </div>
+                </div>
+              </div>
+        </nav>
+        </header>
+
+
+
+        <div class="jumbotron"><p>    </p></br>
+              <h1 class="display-4"style="margin-left:10%">Bienvenue sur mon Portfolio !</h1>
+              <p class="lead"style="margin-left:10%">BTS SIO OPTION SLAM</p>
+        </div>   
+                <section>
+                    <div class="container">
+                        <div class="row">
+                            <div class=center-block style=float:none;></div>
+                                <div class="text-justify col-md-4 bg-light ">
+                                    <h2>Présentation</h2>
+                                    <p>Ce site a été conçu pour vous faire part des projets informatiques sur lesquels j'ai participé et de mettre en avant les différentes compétences que j'ai acquises. </p>                                 
+                                    <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+                                </div>
+                            
+                                <div class="text-justify col-md-4 bg-light">
+                                    <p >        </p></br>
+                                    <p>Actuellement en première année de <a href="http://www.onisep.fr/Ressources/Univers-Formation/Formations/Post-bac/BTS-Services-informatiques-aux-organisations-option-B-solutions-logicielles-et-applications-metiers"><br><strong>BTS Services informatiques aux Organisations option Solutions logicielles et applications métiers(SLAM)<br/></strong></a> Cette formation de 2 ans se déroule dans le Lycée Fulbert de Chartres. </p>
+                                </div>
+                                <div class="text-justify col-md-4 bg-light">
+                                    <p>        </p></br>
+                                    <p>Ce Portfolio à été réalisé en HTML,CSS et Bootstrap. </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </section> 
+
+
+                <section>
+                    <div class="container">
+                        <div class="card" data-aos="fade-up" data-aos-offset="10">
+                            <div class="row">
+                                <div class="col-lg-15 col-md-12">
+                                      <div class="card-header bg-dark" style="max-width: 100%; color: white;">MA FORMATION</div>
+                                    <div class="card-body">
+                                        <div class="h4 mt-0 title"></div>
+                                        <ul>
+                                            <li> • 2019-2021 Préparation au BTS Services Informatiques aux Organisations (SIO), Lycée Fulbert,  Chartres </li>
+                                            <li> • 2019  BTS Management des Unités Commerciales (MUC), Lycée Paul Eluard, Saint Denis  </li>
+                                            <li> • 2017  Baccalauréat ES (Economique et Sociale), Spécialité Economie Approfondie, Lycée Maurice Utrillo, Stains  </li>
+                                        </ul> 
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                
+
+                                                
+
+
+<section>
+<div class="container">
+                <div class="card" data-aos="fade-up" data-aos-offset="10">
+                    <div class="row">
+                        <div class="col-lg-15 col-md-12">
+                            <div class="card-header bg-dark" style="max-width: 100%; color: white;">COMPÉTENCES INFORMATIQUES</div>
+                                <div class="card-body">
+                                    <div class="col-md-12">
+                                     <div class ="competence"style="margin-left:5%;margin-top: 5px;">
+                                    
+                                        <div class="container-info">
+                                          <h7>HTML/CSS</h7>
+                                          <div class="progress">
+                                            <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:50%">
+                                              50% 
+                                            </div>
+                                          </div>
+                                        </div> 
+                                        <div class="container-info" style=>
+                                          <h7>PHP/MYSQL</h7>
+                                          <div class="progress">
+                                            <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:40%">
+                                              40% 
+                                            </div>
+                                          </div>
+                                        </div>
+                                        <div class="container-info">
+                                          <h7>C#</h7>
+                                          <div class="progress">
+                                            <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:30%">
+                                              30%
+                                            </div>
+                                          </div>
+                                        </div>
+                                        <div class="container-info">
+                                          <h7>PYTHON</h7>
+                                          <div class="progress">
+                                            <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:40%">
+                                              40%
+                                            </div>
+                                          </div>
+                                        </div> 
+                                    </div>
+                                </div> 
+                                </div> 
+                                </div> 
+                            </div> 
+                        </div>   
+</div>
+</section>   
+                                        
+        
+                <section>
+                    <div class="container">
+                        <div class="card" data-aos="fade-up" data-aos-offset="10">
+                            <div class="row">
+                                <div class="col-lg-15 col-md-12">
+                                      <div class="card-header bg-dark" style="max-width: 100%; color: white;">LANGUES</div>
+                                    <div class="card-body">
+                                        <div class="h4 text-center title"> </div>
+                                        <ul>
+                                            <li>Anglais-Niveau intermédiaire </li>
+                                            <li>Espagnol Niveau intermédiaire </li>
+                                            <li>Tamoul (Inde) Courant (langue maternelle)</li>
+                                        </ul> 
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <!--<section>
+                    <div class="container">
+                        <div class="card">
+                            <div class="card-header"> 
+                            EXPÉRIENCES PROFESSIONELLES
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">Stage de 14 semaines à ELECTRO DEPOT au rayon MULTIMEDIA</h5>
+                                <p>MISSIONS : </p>
+
+                            </div>
+                        </div>
+                    </div>-->
+                    <div class="mb-4">
+                <h6 class="text-uppercase"></h6>
+                <!-- Solid divider -->
+                <hr class="solid"style="margin-top:50px;margin-left: 10%;margin-right:10%";>
+                </section>
+                    <div class="h4 text-center mb-5 title" style="text-align:center;margin-top:2%";>Expérience professionnelle</div>
+                        <div class="container" style="margin-left:13.5%">
+                            <div class="card"style="margin-right: 2%;">
+                              <div class="row">
+                                <div class="col-md-3 bg-dark" data-aos="fade-up" data-aos-offset="10" data-aos-duration="500">
+                                  <div class="card-body cc-experience-header"style="margin-left:18%; margin-top:50px;margin-bottom: 50px";>
+                                    <div class="h5"style="color:white;text-align: justify;vertical-align:center;margin-top: 30px;"><span class="align-middle">Électro Dépôt</span></div>
+                                    <p style="text-align:center;">2017-2019</p>
+                                  </div>
+                                </div>
+                                <div class="col-md-5">
+                                  <div class="card-body">
+                                    <div class="h5">Équipier Brun/Gris (Multimédia)</div>
+                                    <div class="container">
+                                        <p> MISSIONS :</p>
+                                        <p>- Accueil/Conseil/fidélisation du client </p>
+                                        <p>- Réceptionner les produits </p>
+                                        <p>- Amélioration de la notoriété du magasin </p> 
+                                        <p>- Mettre à jour le Facebook </p>
+                                        </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                         </div>                     
+                </section>
+                        <div class="container" style="margin-left:13.5%">
+                            <div class="card"style="margin-right: 2%;">
+                              <div class="row">
+                                <div class="col-md-3 bg-dark" data-aos="fade-up" data-aos-offset="10" data-aos-duration="500">
+                                  <div class="card-body cc-experience-header"style="margin-left:18%; margin-top:50px;margin-bottom: 50px";>
+                                    <div class="h5"style="color:white;text-align: center;margin-top: 30px;"><span class="align-middle">IKEA</span></div>
+                                    <p style="text-align:center;">Août 2019</p>
+                                  </div>
+                                </div>
+                                <div class="col-md-5">
+                                  <div class="card-body">
+                                    <div class="h5">Vendeur</div>
+                                    <div class="container">
+                                        <p> MISSIONS :</p>
+                                        <p>- Accueil/Conseil/fidélisation du client </p>
+                                        <p>- Assurer la gestion du stock </p>
+                                        <p>- Identifier et cibler le besoin du client </p> 
+                                        </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                         </div>
+                         <div class="container"style="margin-left: center">
+                           <form action="me contacter.php" method="post">
+                             <div>
+                               <label for ="name">Nom : </label>
+                               <input type="text" name="nom"></input><br>
+                               
+                               <label for ="mail">Email : </label>
+                               <input type="text" name="Email"></input><br>
+                               
+                               <label for ="name">Votre message : </label>
+                               <textarea name="message" rows="8" cols="45">
+                               </textarea>
+                               
+                               <input type="submit" value="Valider"/>
+
+
+
+                             </div>
+                           </form>
+                           <p>Vous pouvez <a href="me contacter.php">cliquer ici</a> pour aller à la page suivante.</p>
+
+
+                         </div>
+
+
+
+                                       
+                                    <footer>
+                                        <p style="text-align:center"> Me contacter </p>
+                                    </footer>
+                         
+    </body> 
+
+    
+
+</html> 
